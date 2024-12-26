61bd7d3ad385a1058a73f07a1e1eae6be522dcce
fepskieee <120133472+fepskieee@users.noreply.github.com>
2024-12-26 11:49:45 +0800

    chore: fix vite config file
       + added root path
       + modify to the correct include & exclude test path globs

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from "vitest/config"
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
+  root: path.resolve(__dirname, "./"),
   test: {
     globals: true,
     environment: "node",
-    include: ["tests/**/*.js", "src/**/*.js"],
-    exclude: [
-      "**/node_modules/**",
-      "**/.redis.drawio/**",
-      "**/.{git}/**",
-      "**/{vite,vitest}.config.*",
-    ],
+    include: ["**/*.{test, spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: ["**/node_modules/**"],
   },
 })
