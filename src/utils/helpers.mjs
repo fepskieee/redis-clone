365c6a8da49c38aee29cb6c84694fc87656b8a69
fepskieee <120133472+fepskieee@users.noreply.github.com>
2024-12-28 23:35:02 +0800

    feat: add helpers for timestamp, namespace, ipv4

---
+++ src/utils/helpers.mjs
@@ -0,0 +1,16 @@
+import path from "path"
+import { fileURLToPath } from "url"
+
+export const getServerTimestamp = new Date().toISOString()
+
+export const getFilename = (url) => {
+  const __filename = fileURLToPath(url)
+
+  return path.basename(__filename, path.extname(__filename))
+}
+
+export const getIPv4 = (address) => {
+  return address.startsWith("::ffff:") || address.startsWith("::1:")
+    ? address.slice(7)
+    : address
+}
