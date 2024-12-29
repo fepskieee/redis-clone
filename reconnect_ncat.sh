8eceee6253f4c8ac2ba85ff996b446fb1f45cf56
fepskieee <120133472+fepskieee@users.noreply.github.com>
2024-12-30 04:17:38 +0800

    fix: shell script of twice connecting

--- reconnect_ncat.sh
+++ reconnect_ncat.sh
@@ -3,28 +3,29 @@
 SERVER="127.0.0.1"
 PORT="6379"
 
-TIMEOUT=60
+TIMEOUT=30
 INTERVAL=2
 START_TIME=$(date +%s)
 
-
-echo -e "Connecting to Nedis $SERVER:$PORT...\n"
+echo -e "Connecting to Nedis $SERVER:$PORT..."
 
 while true; do
-  ncat -z $SERVER $PORT
+  netstat -an | grep ":$PORT" | grep -q "LISTEN"
+  NETSTAT_EXIT_STATUS=$? 
 
-  if [ $? -eq 0 ]; then
-    ncat $SERVER $PORT 2>ncat_logs.txt
+  if [ $NETSTAT_EXIT_STATUS -eq 0 ]; then
+    ncat $SERVER $PORT 2>> ncat.log
     START_TIME=$(date +%s)
+    echo -e "Lost connection to Nedis $SERVER:$PORT..."
   else
     CURRENT_TIME=$(date +%s)
     ELAPSED_TIME=$((CURRENT_TIME - START_TIME))
-    
+      
     if [ $ELAPSED_TIME -ge $TIMEOUT ]; then
       echo -e "\nCannot connect to Nedis $SERVER:$PORT after $TIMEOUT seconds.\n"
       exit 0
     else
-      echo -e "Failed to connect Nedis $SERVER:$PORT. Retrying in $INTERVAL seconds..."
+      echo -e "\nFailed to connect Nedis $SERVER:$PORT. Retrying in $INTERVAL seconds..."
       sleep $INTERVAL
     fi
   fi
