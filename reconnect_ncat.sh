1a971ac0d86eae9fc50fa3998ab1226ef3f56563
fepskieee <120133472+fepskieee@users.noreply.github.com>
2024-12-30 16:32:39 +0800

    refactor: sanitize messages

--- reconnect_ncat.sh
+++ reconnect_ncat.sh
@@ -4,10 +4,15 @@ SERVER="127.0.0.1"
 PORT="6379"
 
 TIMEOUT=30
-INTERVAL=2
+INTERVAL=3
 START_TIME=$(date +%s)
 
-echo -e "Connecting to Nedis $SERVER:$PORT..."
+MSG_CONNECT_SERVER="Connecting to Nedis $SERVER:$PORT..."
+MSG_RECON_SERVER="\nFailed to connect Nedis $SERVER:$PORT. Retrying in $INTERVAL seconds..."
+MSG_CONNECTION_STATUS_SERVER="Lost connection to Nedis $SERVER:$PORT..."
+MSG_OFFLINE_SERVER="\nCannot connect to Nedis $SERVER:$PORT after $TIMEOUT seconds...\n"
+
+echo -e $MSG_CONNECT_SERVER
 
 while true; do
   netstat -an | grep ":$PORT" | grep -q "LISTEN"
@@ -16,16 +21,16 @@ while true; do
   if [ $NETSTAT_EXIT_STATUS -eq 0 ]; then
     ncat $SERVER $PORT 2>> ncat.log
     START_TIME=$(date +%s)
-    echo -e "Lost connection to Nedis $SERVER:$PORT..."
+    echo -e $MSG_CONNECTION_STATUS_SERVER
   else
     CURRENT_TIME=$(date +%s)
     ELAPSED_TIME=$((CURRENT_TIME - START_TIME))
       
     if [ $ELAPSED_TIME -ge $TIMEOUT ]; then
-      echo -e "\nCannot connect to Nedis $SERVER:$PORT after $TIMEOUT seconds.\n"
+      echo -e $MSG_OFFLINE_SERVER
       exit 0
     else
-      echo -e "\nFailed to connect Nedis $SERVER:$PORT. Retrying in $INTERVAL seconds..."
+      echo -e $MSG_RECON_SERVER
       sleep $INTERVAL
     fi
   fi
