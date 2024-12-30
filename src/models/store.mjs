a114ee07b5acdca0568cfba254518d4ad4c91226
fepskieee <120133472+fepskieee@users.noreply.github.com>
2024-12-30 23:28:09 +0800

    feat: add models for store map

---
+++ src/models/store.mjs
@@ -0,0 +1,26 @@
+const storeMap = new Map()
+
+export const get = (key) => {
+  return storeMap.get(key)
+}
+
+export const set = (key, values) => {
+  return storeMap.set(key, values)
+}
+
+export const del = (key) => {
+  return storeMap.delete(key)
+}
+
+export const keys = () => {
+  return structuredClone(storeMap)
+}
+
+const store = () => ({
+  get,
+  set,
+  del,
+  keys,
+})
+
+export default store
