74252f262409c8e2fb390ae17b9fd40ac0b56fb8
fepskieee <120133472+fepskieee@users.noreply.github.com>
2025-01-01 21:37:15 +0800

    feat(services/strings): add incrby method with error handling

--- src/services/Strings.mjs
+++ src/services/Strings.mjs
@@ -10,34 +10,42 @@ class Strings {
   static MAX_MEMORY = 512 * 1024 * 1024
   static MSG_SUCCESS = "+OK\r\n"
   static MSG_EMPTY = "$-1\r\n"
+  static ERR_MSG_SYNTAX_ERROR = "-ERR syntax error\r\n"
   static ERR_MSG_GET_WRONG_NUMBER_ARGS =
     "-ERR Wrong number of arguments for 'GET' command\r\n"
   static ERR_MSG_SET_WRONG_NUMBER_ARGS =
     "-ERR Wrong number of arguments for 'SET' command\r\n"
   static ERR_MSG_MGET_WRONG_NUMBER_ARGS =
     "-ERR Wrong number of arguments for 'MGET' command\r\n"
-  static ERR_MSG_INCR_VALUE_NOT_VALID =
+  static ERR_MSG_VALUE_NOT_INTEGER =
     "-ERR value is not an integer or out of range\r\n"
-  static ERR_MSG_WRONG_TYPE_OEPRATION =
+  static ERR_MSG_INCRBY_WRONG_NUMBER_ARGS =
+    "-ERR Wrong number of arguments for 'INCRBY' command\r\n"
+  static WRONGTYPE_MSG_OEPRATION =
     "-WRONGTYPE Operation against a key holding string value\r\n"
   static ERR_MSG_KEY_EXISTS = "-ERR Key already exists\r\n"
   static ERR_MSG_NOT_STRING = "-ERR Value is not a string\r\n"
   static ERR_MSG_EXCEED_512MB = "-ERR Exceeds memory limit of 512MB\r\n"
 
   static GET([key], category) {
+    if (arguments[0].length > 1) {
+      stringsLogger.error(Strings.ERR_MSG_SYNTAX_ERROR)
+      return Strings.ERR_MSG_SYNTAX_ERROR
+    }
+
     if (!key) {
       stringsLogger.error(Strings.ERR_MSG_GET_WRONG_NUMBER_ARGS)
       return Strings.ERR_MSG_GET_WRONG_NUMBER_ARGS
     }
 
     if (!store.has(key)) {
-      stringsLogger.error(Strings.MSG_EMPTY)
+      stringsLogger.info(Strings.MSG_EMPTY)
       return Strings.MSG_EMPTY
     }
 
     if (store.get(key).type !== category) {
-      stringsLogger.error(Strings.ERR_MSG_WRONG_TYPE_OEPRATION)
-      return Strings.ERR_MSG_WRONG_TYPE_OEPRATION
+      stringsLogger.error(Strings.WRONGTYPE_MSG_OEPRATION)
+      return Strings.WRONGTYPE_MSG_OEPRATION
     }
 
     const result = store.get(key).value
@@ -107,6 +115,11 @@ class Strings {
   }
 
   static SETNX([key, value], category) {
+    if (arguments[0].length > 2) {
+      stringsLogger.error(Strings.ERR_MSG_SYNTAX_ERROR)
+      return Strings.ERR_MSG_SYNTAX_ERROR
+    }
+
     if (store.has(key)) {
       stringsLogger.error(Strings.ERR_MSG_KEY_EXISTS)
       return Strings.ERR_MSG_KEY_EXISTS
@@ -136,20 +149,30 @@ class Strings {
   }
 
   static INCR([key], category) {
+    if (arguments[0].length > 1) {
+      stringsLogger.error(Strings.ERR_MSG_SYNTAX_ERROR)
+      return Strings.ERR_MSG_SYNTAX_ERROR
+    }
+
+    if (!key) {
+      stringsLogger.error(Strings.ERR_MSG_GET_WRONG_NUMBER_ARGS)
+      return Strings.ERR_MSG_GET_WRONG_NUMBER_ARGS
+    }
+
     if (!store.has(key)) {
       store.set(key, { type: category, value: "0" })
     }
 
     if (store.get(key).type !== category) {
-      stringsLogger.error(Strings.ERR_MSG_WRONG_TYPE_OEPRATION)
-      return Strings.ERR_MSG_WRONG_TYPE_OEPRATION
+      stringsLogger.error(Strings.WRONGTYPE_MSG_OEPRATION)
+      return Strings.WRONGTYPE_MSG_OEPRATION
     }
 
-    let value = parseInt(store.get(key).value)
+    let value = Number(store.get(key).value)
 
     if (!Number.isInteger(value)) {
-      stringsLogger.error(Strings.ERR_MSG_INCR_VALUE_NOT_VALID)
-      return Strings.ERR_MSG_INCR_VALUE_NOT_VALID
+      stringsLogger.error(Strings.ERR_MSG_VALUE_NOT_INTEGER)
+      return Strings.ERR_MSG_VALUE_NOT_INTEGER
     }
 
     value = value + 1
@@ -158,18 +181,35 @@ class Strings {
     return `:${value}\r\n`
   }
 
-  static INCRBY([key, increment]) {
+  static INCRBY([key, increment], category) {
+    if (arguments[0].length > 2) {
+      stringsLogger.error(Strings.ERR_MSG_SYNTAX_ERROR)
+      return Strings.ERR_MSG_SYNTAX_ERROR
+    }
+
+    if (!key || !increment) {
+      stringsLogger.error(Strings.ERR_MSG_INCRBY_WRONG_NUMBER_ARGS)
+      return Strings.ERR_MSG_INCRBY_WRONG_NUMBER_ARGS
+    }
+
+    if (!Number.isInteger(Number(increment))) {
+      stringsLogger.error(Strings.ERR_MSG_VALUE_NOT_INTEGER)
+      return Strings.ERR_MSG_VALUE_NOT_INTEGER
+    }
+
     if (!store.has(key)) {
-      store.set(key, { type: "string", value: "0" })
+      store.set(key, { type: category, value: "0" })
     }
 
-    if (store.get(key).type !== "string") {
-      stringsLogger.error(Strings.ERR_MSG_WRONG_TYPE_OEPRATION)
-      return Strings.ERR_MSG_WRONG_TYPE_OEPRATION
+    let value = parseInt(store.get(key).value)
+
+    if (!Number.isInteger(value)) {
+      stringsLogger.error(Strings.ERR_MSG_VALUE_NOT_INTEGER)
+      return Strings.ERR_MSG_VALUE_NOT_INTEGER
     }
 
-    const value = parseInt(store.get(key).value) + parseInt(increment)
-    store.set(key, { type: "string", value: value.toString() })
+    value = parseInt(value) + parseInt(increment)
+    store.set(key, { ...store.get(key), value: value.toString() })
 
     return `:${value}\r\n`
   }
