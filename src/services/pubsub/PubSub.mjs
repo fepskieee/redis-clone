21b3ab2820616ed56b415370683a88de57d4a677
fepskieee <120133472+fepskieee@users.noreply.github.com>
2025-01-03 22:39:18 +0800

    refactor(pubsub): update subscriber and channel handling to use maps for improved performance

--- src/services/pubsub/PubSub.mjs
+++ src/services/pubsub/PubSub.mjs
@@ -1,60 +1,61 @@
-import { subscribers } from "../../models/subscribers.mjs"
-import { channels } from "../../models/subscribers.mjs"
+import subscribersMap from "../../models/subscribers.mjs"
+import channelsMap from "../../models/subscribers.mjs"
 
 export default class PubSub {
   static SUBSCRIBE(args, type, socket) {
     const [channel] = args
 
-    if (!subscribers.has(channel)) {
-      subscribers.set(channel, new Set())
+    if (!subscribersMap.has(channel)) {
+      subscribersMap.set(channel, new Set())
     }
-    subscribers.get(channel).add(socket)
+    subscribersMap.get(channel).add(socket)
 
-    if (!channels.has(socket)) {
-      channels.set(socket, new Set())
+    if (!channelsMap.has(socket)) {
+      channelsMap.set(socket, new Set())
     }
-    channels.get(socket).add(channel)
+    channelsMap.get(socket).add(channel)
 
     return `+Subscribed to ${channel}\r\n`
   }
 
-  static PUBLISH(args) {
+  static PUBLISH(args, type, socket) {
+    console.log("publish")
     const [channel, message] = args
-    const subs = subs.get(channel)
+    const subscribers = subscribersMap.get(channel)
 
-    if (!subs) {
+    if (!subscribers) {
       return `:0\r\n`
     }
 
-    subs.forEach((socket) => {
+    subscribers.forEach((socket) => {
       socket.write(
         `*3\r\n$7\r\nmessage\r\n$${channel.length}\r\n${channel}\r\n$${message.length}\r\n${message}\r\n`
       )
     })
 
-    return `:${subs.size}\r\n`
+    return `:${subscribers.size}\r\n`
   }
 
   static UNSUBSCRIBE(args, type, socket) {
     const [channel] = args
 
-    if (subscribers.has(channel)) {
-      subscribers.get(channel).delete(socket)
+    if (subscribersMap.has(channel)) {
+      subscribersMap.get(channel).delete(socket)
     }
 
-    if (channels.has(socket)) {
-      channels.get(socket).delete(channel)
+    if (channelsMap.has(socket)) {
+      channelsMap.get(socket).delete(channel)
     }
 
     return `+Unsubscribed from ${channel}\r\n`
   }
 
   static handleDisconnect(socket) {
-    if (channels.has(socket)) {
-      const channels = this.channels.get(socket)
+    if (channelsMap.has(socket)) {
+      const channels = channelsMap.get(socket)
       channels.forEach((channel) => {
-        if (subscribers.has(channel)) {
-          subscribers.get(channel).delete(socket)
+        if (subscribersMap.has(channel)) {
+          subscribersMap.get(channel).delete(socket)
         }
       })
       channels.delete(socket)
