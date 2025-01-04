97e79afaca0340756e39a03c71f507d84f66fdd4
fepskieee <120133472+fepskieee@users.noreply.github.com>
2025-01-05 00:08:24 +0800

    refactor: rename filenames and change directory

---
+++ src/utils/constants/messages/generic-messages.js
@@ -0,0 +1,6 @@
+const ERR_MSG_EXCEED_512MB = "Exceeds limit of 512MB"
+const ERR_MSG_VALUE_NOT_STRING = "Value is not a string"
+const ERR_MSG_VALUE_NOT_INTEGER = "Value is not an integer"
+const ERR_MSG_WRONG_NUMBER_ARGS = "Wrong number of arguments"
+const ERR_MSG_WRONGTYPE_OPERATION =
+  "Operation against a key holding different values"
---
+++ src/utils/constants/messages/strings-messages.js
@@ -0,0 +1,1 @@
+const ERR_MSG = ""
