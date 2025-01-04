061055cd0c7703586925efded35c84c057ec8c88
fepskieee <120133472+fepskieee@users.noreply.github.com>
2025-01-04 23:53:33 +0800

    feat(utils/nesp): add utility functions for response formatting

---
+++ src/utils/nesp.js
@@ -0,0 +1,15 @@
+export const reply = {
+  simpleString: (strings = "OK") => `+${strings}\r\n`,
+  simpleError: (strings = "ERR") => `-${strings}\r\n`,
+  integer: (integer) => `:${integer}\r\n`,
+  bulkString: (strings) => `$${strings.length}\r\n${strings}\r\n`,
+  array: (array) => {
+    return `${array.reduce(
+      (accumulator, currentValue) => accumulator + `$${currentValue}\r\n`,
+      `*${array.length}\r\n`
+    )}`
+  },
+  null: () => `_\r\n`,
+  boolean: (boolean) => `#${boolean ? "t" : "f"}\r\n`,
+  double: (double) => `,${double}\r\n`,
+}
