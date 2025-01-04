f0dd680167c88c04b4b7d3e67bbbf5ea7c6297e2
fepskieee <120133472+fepskieee@users.noreply.github.com>
2025-01-05 00:24:18 +0800

    refactor(utils/constants): add named export to each message constant

--- src/utils/constants/generic-messages.js
+++ src/utils/constants/generic-messages.js
@@ -1,6 +1,7 @@
-const ERR_MSG_EXCEED_512MB = "Exceeds limit of 512MB"
-const ERR_MSG_VALUE_NOT_STRING = "Value is not a string"
-const ERR_MSG_VALUE_NOT_INTEGER = "Value is not an integer"
-const ERR_MSG_WRONG_NUMBER_ARGS = "Wrong number of arguments"
-const ERR_MSG_WRONGTYPE_OPERATION =
+export const ERR_MSG_EXCEED_512MB = "Exceeds limit of 512MB"
+export const ERR_MSG_SYNTAX_ERROR = "Syntax error"
+export const ERR_MSG_VALUE_NOT_STRING = "Value is not a string"
+export const ERR_MSG_VALUE_NOT_INTEGER = "Value is not an integer"
+export const ERR_MSG_WRONG_NUMBER_ARGS = "Wrong number of arguments"
+export const ERR_MSG_WRONGTYPE_OPERATION =
   "Operation against a key holding different values"
