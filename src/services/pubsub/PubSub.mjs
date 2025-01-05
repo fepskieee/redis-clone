8153d6bc9056a7daed68f18ba3932cfb6458003d
fepskieee <120133472+fepskieee@users.noreply.github.com>
2025-01-05 21:31:45 +0800

    chore(services/pubsub): sanitize comments

--- src/services/pubsub/PubSub.mjs
+++ src/services/pubsub/PubSub.mjs
@@ -11,11 +11,6 @@ export default class PubSub extends EventEmitter {
   static SUBSCRIBE(args, type, socket) {
     const [topic] = args
 
-    socket.isSubscriber = true
-    // socket.isInSubscribeMode = true
-    // socket.isPubSubClient = true
-    // socket.subscribedCount = (socket.subscribedCount || 0) + 1
-
     if (!subscriberMap.has(topic)) {
       subscriberMap.set(topic, new Set())
     }
@@ -26,39 +21,6 @@ export default class PubSub extends EventEmitter {
     }
     channels.get(socket).add(topic)
 
-    // this.emitter.on("message", (topic, message) => {
-    //   console.log("i am emitting to", topic, message)
-    //   socket.write(
-    //     `*3\r\n$7\r\nmessage\r\n$${topic.length}\r\n${topic}\r\n$${message.length}\r\n${message}\r\n`
-    //   )
-    // })
-    // subscriberMap.get(topic).forEach((subscriber) => {
-    //   console.log(topic)
-    //   subscriber.write(
-    //     `*3\r\n$7\r\nmessage\r\n$${topic.length}\r\n${topic}\r\n$${message.length}\r\n${message}\r\n`
-    //   )
-    // })
-
-    // this.emitter.on("message", (topic, message) => {
-    //   subscribers.get(topic).forEach((client) => {
-    //     console.log("subscribe")
-    //     client.write(
-    //       `*3\r\n$7\r\nmessage\r\n$${topic.length}\r\n${topic}\r\n$${message.length}\r\n${message}\r\n`
-    //     )
-    //   })
-    // })
-
-    // this.emitter.on("message", (channel, message) => {
-    //   if (topic === channel) {
-    //     socket.write(
-    //       `*3\r\n$9\r\nsubscribe\r\n$${topic.length}\r\n${topic}\r\n:${
-    //         subscribers.get(topic).size
-    //       }\r\n`
-    //     )
-    //   }
-    // })
-
-    // return `+OK\r\n`
     return `*3\r\n$9\r\nsubscribe\r\n$${topic.length}\r\n${topic}\r\n:${
       channels.get(socket).size
     }\r\n`
@@ -72,7 +34,6 @@ export default class PubSub extends EventEmitter {
       return `:0\r\n`
     }
 
-    // this.emitter.emit("message", topic, message)
     subscribers.forEach((subscriber) => {
       console.log("publish")
       subscriber.write(
