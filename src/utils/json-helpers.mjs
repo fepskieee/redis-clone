7d1c07f619c5c9d24c8344e6d9d5acaee6f0aa34
fepskieee <120133472+fepskieee@users.noreply.github.com>
2025-01-08 22:52:55 +0800

    refactor: json cleanup

--- src/utils/json-helpers.mjs
+++ src/utils/json-helpers.mjs
@@ -14,26 +14,6 @@ export const _pathExists = (path, json) => {
 export const _setValueAtPath = (json, path, value) => {
   jp.value(json, path, value)
   return json
-  // const pathSegments = path.slice(1).split(".").filter(Boolean)
-  // let current = json
-
-  // if (pathSegments.length === 0) {
-  //   return value
-  // }
-
-  // pathSegments.forEach((segment) => {
-  //   if (!current[segment] || typeof current[segment] !== "object") {
-  //     if (!_pathExists(path, json)) {
-  //       current[segment] = {}
-  //     }
-  //   }
-  //   current = current[segment]
-  // })
-
-  // const lastSegment = pathSegments[pathSegments.length - 1]
-  // current[lastSegment] = value
-
-  // return json
 }
 
 export const _getValueAtPath = (json, paths) => {
@@ -43,20 +23,6 @@ export const _getValueAtPath = (json, paths) => {
     path = path[0] === "$" ? path : `$${path}`
     return jp.query(json, path)
   })
-
-  // const pathSegments = paths[0].slice(1).split(".").filter(Boolean)
-
-  // let current = json
-  // for (const segment of pathSegments) {
-  //   if (
-  //     !current ||
-  //     typeof current !== "object" ||
-  //     !current.hasOwnProperty(segment)
-  //   ) {
-  //     return undefined
-  //   }
-  //   current = current[segment]
-  // }
 
   return results
 }
